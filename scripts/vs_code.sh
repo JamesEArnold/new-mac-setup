d8e32d7c49c33e5fe8e303f4ec3f1ccdb8ddb640
JamesEArnold <jearnold22@gmail.com>
2024-08-14 20:12:11 -0400

    update vscode extensions

--- scripts/vs_code.sh
+++ scripts/vs_code.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Ask for the administrator password upfront.
+echo "Requesting administrator password..."
+sudo -v
+if [ $? -eq 0 ]; then
+    echo "Administrator password accepted."
+else
+    echo "Failed to obtain administrator privileges."
+    exit 1
+fi
+
 # Function to check if VSCode is installed
 check_vscode_installed() {
   if ! command -v code &> /dev/null; then
@@ -32,5 +42,12 @@ install_vscode_extension bradlc.vscode-tailwindcss # Tailwind Intellisense
 install_vscode_extension wayou.vscode-todo-highlight # TODO Highlights
 install_vscode_extension esbenp.prettier-vscode # Prettier
 install_vscode_extension pkief.material-icon-theme # Material Theme Icons
+install_vscode_extension streetsidesoftware.code-spell-checker # Spell Checker
+install_vscode_extension donjayamanne.git-extension-pack # Gitblame, Gitignore, line history
+install_vscode_extension wix.vscode-import-cost # Import size
+install_vscode_extension ms-vsliveshare.vsliveshare # Live share pair programming
+install_vscode_extension ms-playwright.playwright # Playwright runner
+install_vscode_extension prisma.prisma # Prisma intellisense
+install_vscode_extension sonarsource.sonarlint-vscode # Sonar Linting
 
 echo "All extensions installed successfully!"
